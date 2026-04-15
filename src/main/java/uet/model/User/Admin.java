50acc845bb2c669d036af14ebf1d62f511bd9e6d
Nguyen Quoc Dung <nqdunggqhung@gmail.com>
2026-04-15 14:13:28 +0700

    Commit nqd

--- src/main/java/uet/model/User/Admin.java
+++ src/main/java/uet/model/User/Admin.java
@@ -1,4 +1,11 @@
 package uet.model.User;
 
 public class Admin {
+    private User user; // Admin đại diện cho User này
+    public Admin(User user) {
+        this.user = user;
+    }
+    public String getType(){
+        return "Admin";
+    }
 }

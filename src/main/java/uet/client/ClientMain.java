e0019ac7b14a33e8d8e83894747c444cc99fac18
Nguyen Duc Anh <nguyenducanhhihi7@gmail.com>
2026-04-18 11:37:26 +0700

    nda commit

--- src/main/java/uet/client/ClientMain.java
+++ src/main/java/uet/client/ClientMain.java
@@ -1,8 +1,11 @@
 package uet.client;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +22,10 @@ public class ClientMain extends Application {
         // Load màn hình đăng nhập đầu tiên
         switchTo("LoginView.fxml", 400, 400);
         window.show();
+        window.setOnCloseRequest(e ->{
+            e.consume();  // giúp cửa sổ không bị tắt đột ngột
+            exit();
+        });
     }
 
     // Hàm tiện ích để các Controller gọi khi cần chuyển màn hình
@@ -33,7 +40,17 @@ public class ClientMain extends Application {
             System.err.println("Không thể tải giao diện: " + fxmlFile);
         }
     }
-
+    public static void exit (){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("You are about to Exit.");
+        alert.setContentText("Do you want to Exit app .");
+        if (alert.showAndWait().get() == ButtonType.OK){
+            window.close();
+            Platform.exit();    // Dừng bộ máy JavaFX
+            System.exit(0);     // Tắt hoàn toàn chương trình (Kill process)
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
